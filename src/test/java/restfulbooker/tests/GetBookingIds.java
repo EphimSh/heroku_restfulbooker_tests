a21b93fef4598515d5b99e2c77b2051d0b854f3a
EphimSh <ephimizm@gmail.com>
2023-10-08 18:48:29 +0300

    refactored: check type of data; id uniqueness

--- src/test/java/restfulbooker/tests/GetBookingIds.java
+++ src/test/java/restfulbooker/tests/GetBookingIds.java
@@ -7,12 +7,16 @@ import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import restfulbooker.models.GetBookingIdResponseModel;
+import restfulbooker.tests.TestBase;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static restfulbooker.specs.Specifications.getBookingIdsWithStatusCode200;
 import static restfulbooker.specs.Specifications.requestSpec;
 
@@ -35,8 +39,34 @@ public class GetBookingIds extends TestBase {
                         .jsonPath()
                         .getList(".", GetBookingIdResponseModel.class));
 
-        step("Assert that list is not empty", () -> {
-            assertFalse(response.isEmpty());
+        step("Check that each id is unique", () -> {
+            Set<Integer> uniqueIds = new HashSet<>();
+            for (GetBookingIdResponseModel booking : response) {
+                uniqueIds.add(booking.getBookingId());
+            }
+            assertEquals(response.size(), uniqueIds.size());
+        });
+    }
+
+    @Test
+    @Story("Get booking")
+    @Description("Test retrieving a list of booking IDs and checking their type")
+    @DisplayName("Check Booking ID Types")
+    void checkBookingIdTypes(){
+        List<GetBookingIdResponseModel> response = step("Get list of booking ids", () ->
+                given(requestSpec)
+                        .when()
+                        .get("/booking")
+                        .then()
+                        .spec(getBookingIdsWithStatusCode200)
+                        .extract()
+                        .jsonPath()
+                        .getList(".", GetBookingIdResponseModel.class));
+
+        step("Check that each id is an Integer type", () -> {
+            for (GetBookingIdResponseModel booking : response) {
+                assertInstanceOf(Integer.class, booking.getBookingId());
+            }
         });
     }
 }
